b2a347bcb42bbd7572ac1c21843102ce36021ff7
manaals <manaals@cep.org>
2025-06-25 18:48:14 -0400

    updata tables

--- sql/init.sql
+++ sql/init.sql
@@ -13,7 +13,7 @@ CREATE TABLE cep_core_todo.surveys_list (
 	last_pulled_ts timestamp NULL,
 	"comment" text NULL,
 	s3_last_modified_ts timestamp NULL,
-	is_working bool DEFAULT false NULL,
+	is_working bool DEFAULT false,
 	definition_in_s3 bool DEFAULT false NULL,
 	definition_in_postgres bool DEFAULT false NULL,
 	responses_in_s3 bool DEFAULT false NULL,
@@ -37,13 +37,45 @@ CREATE TABLE cep_core_todo.dashboard_list (
 	CONSTRAINT dashboard_list_dashboard_id_key UNIQUE (dashboard_id)
 );
 
-DROP TABLE IF EXISTS cep_core_todo.comment_files_list;
-CREATE TABLE cep_core_todo.comment_files_list (
-	salesforce_object_id text NOT NULL,
-	salesforce_object_type text NOT NULL,
+DROP TABLE IF EXISTS cep_core_todo.profiled_comment_files_list;
+CREATE TABLE cep_core_todo.profiled_comment_files_list (
+    profiled_id text NOT NULL,
     "level" text NOT NULL,
     product text NOT NULL,
-	parquet_uploaded bool DEFAULT false NULL,
-	excel_uploaded bool DEFAULT false NULL,
-	CONSTRAINT comment_files_list_salesforce_object_id_product_level_key UNIQUE(salesforce_object_id, product, level)
+    is_working bool DEFAULT false,
+    parquet_uploaded bool DEFAULT false NULL,
+    excel_uploaded bool DEFAULT false NULL,
+	last_updated_timestamp timestamp NULL,
+	s3_last_modified_ts timestamp NULL,
+	needs_to_be_created bool DEFAULT true NULL,
+    CONSTRAINT comment_files_list_profiled_id_product_level_key UNIQUE(profiled_id, product, level)
+);
+
+DROP TABLE IF EXISTS cep_core_todo.opportunity_product_level_comment_files_list;
+CREATE TABLE cep_core_todo.opportunity_product_level_comment_files_list (
+    opportunity_id text NOT NULL,
+    "level" text NOT NULL,
+    product text NOT NULL,
+    is_working bool DEFAULT false,
+    parquet_uploaded bool DEFAULT false NULL,
+    excel_uploaded bool DEFAULT false NULL,
+	last_updated_timestamp timestamp NULL,
+	s3_last_modified_ts timestamp NULL,
+	needs_to_be_created bool DEFAULT true NULL,
+    CONSTRAINT comment_files_list_opportunity_id_product_level_key UNIQUE(opportunity_id, product, level)
+);
+
+DROP TABLE IF EXISTS cep_core_todo.opportunity_product_comment_files_list;
+CREATE TABLE cep_core_todo.opportunity_product_comment_files_list (
+    opportunity_id text NOT NULL,
+	product text NOT NULL,
+    is_working bool DEFAULT false,
+    concerning_parquet_uploaded bool DEFAULT false,
+    concerning_excel_uploaded bool DEFAULT false,
+	additional_info_parquet_uploaded bool DEFAULT false,
+	additional_info_excel_uploaded bool DEFAULT false,
+	last_updated_timestamp timestamp NULL,
+	s3_last_modified_ts timestamp NULL,
+	needs_to_be_created bool DEFAULT true NULL,
+    CONSTRAINT comment_files_list_opportunity_id_product_key UNIQUE(opportunity_id, product)
 );
